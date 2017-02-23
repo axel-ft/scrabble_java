ddb11aaf5026177e9fbdd6c788194303ded2be67
steodec <paul.tedesco@live.fr>
2017-02-23 16:03:20 +0100

    Add letters + point's letters

---
+++ src/com/scrabble/Main.java
@@ -0,0 +1,19 @@
+package com.scrabble;
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.Override;
+import javax.swing.JButton;
+
+/**
+ * Created by pault on 23/02/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Letter lettres = new Letter();
+        lettres.displayLettre();
+    }
+}
---
+++ src/com/scrabble/Player.java
@@ -0,0 +1,12 @@
+package com.scrabble;
+
+/**
+ * Created by pault on 23/02/2017.
+ */
+public class Player {
+    public void createPlayer()
+    {
+
+    }
+
+}
