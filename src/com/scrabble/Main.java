9a12658af7d254be265c6c8feedccb60f9cdcde6
axel-ft <axel.floquettrillot@ynov.com>
2017-02-28 14:16:16 +0100

    Blank window added

--- src/com/scrabble/Main.java
+++ src/com/scrabble/Main.java
@@ -1,14 +1,20 @@
 package com.scrabble;
 
-import java.util.*;
 import java.lang.String;
+import javax.swing.JFrame;
 
 /**
  * Created by pault on 23/02/2017.
  */
 public class Main {
-
 	public static void main(String[] args) {
+		JFrame window = new JFrame();
+	    window.setTitle("Scrabble");
+	    window.setSize(700, 700);
+	    window.setLocationRelativeTo(null);
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.setVisible(true);
+	    
 		Draw pioche = new Draw();
 		pioche.displayPawns();
 		Player player1 = new Player();
